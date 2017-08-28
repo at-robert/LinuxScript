3c0a4f857f1e14994fd52f8019e654d1c31af581
robert.lo <atttrobert@gmail.com>
2017-08-28 22:39:01 +0800

    [import] - To add adv_redirect.sh

---
+++ adv_exec/adv_redirect.sh
@@ -0,0 +1,9 @@
+PATH=~/Documents/Github/LinuxScript/adv_exec
+python $PATH/adv_redirect.py $1
+
+# if [ "$1" == "" ];
+# then
+#     python $PATH/adv_redirect.py
+# else
+#     python $PATH/adv_redirect.py $1
+# fi
